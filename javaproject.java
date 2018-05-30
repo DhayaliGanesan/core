d92adc712f26198f3bf86f3c77ac5fc96189e02b
DhayaliGanesan <deepadhayali13@gmail.com>
2018-05-30 02:50:52 -0700

    op

---
+++ javaproject.java
@@ -0,0 +1,14 @@
+package javaproject;
+
+public class javaproject{ 
+	public static void main (String S[]) {
+	int count=10;
+    if(count < 20)
+       System.out.println(10);
+    else {
+       System.out.println(20);
+      }
+     
+    int response = count > 10 ? 100 : 10;
+}
+}
