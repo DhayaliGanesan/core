3b5a91edc39acb898f30a41b6b2c95699f1a0d0f
DhayaliGanesan <deepadhayali13@gmail.com>
2018-05-30 02:36:17 -0700

    con

---
+++ Condition.java
@@ -0,0 +1,10 @@
+public class Condition{
+public static void main(String[] args ){
+   int count=10;
+    if(count < 20)
+       System.out.println(10);
+    else(count > 20)
+       System.out.println(20);
+      }
+    
+}
