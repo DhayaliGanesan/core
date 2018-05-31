d47c96a6e80416f3dd1edeec268a2123fde59d80
DhayaliGanesan <deepadhayali13@gmail.com>
2018-05-31 02:10:27 -0700

    month

---
+++ javaproject7.java
@@ -0,0 +1,13 @@
+package javaproject;
+
+public class javaproject7 {
+    public static void main(String args[]){
+    Date d1 = new Date(0,0,0);
+    d1.setDay(31);
+    d1.setMonth(5);
+    d1.setYear(2018);
+    d1.displayDate();
+    Date d2 = new Date(1,2018,6);
+    d2.displayDate();
+    }
+}
