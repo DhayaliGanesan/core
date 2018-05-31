77a1991ff2359f4d5b9e95f90b90c138a672e93b
DhayaliGanesan <deepadhayali13@gmail.com>
2018-05-30 23:17:51 -0700

    forwhile

---
+++ javaproject4.java
@@ -0,0 +1,16 @@
+package javaproject;
+
+public class javaproject4{
+	public static void main(String S[]){
+		int i = 0;
+		while(i<10)
+	{
+		System.out.println("i=" +i);
+		i++;
+	}
+		  for(i=0;i<10;i++) {
+				System.out.println(i);
+}
+	}
+}
+
