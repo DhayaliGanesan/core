4eefdce954b399532a0a7fd7c63c1de605e04a6a
DhayaliGanesan <deepadhayali13@gmail.com>
2018-06-01 05:05:35 -0700

    args

---
+++ java11.java
@@ -0,0 +1,15 @@
+package javaproject;
+
+public class java11 {
+	static void main (String args[]){
+		int sum = 0;
+		String sum1 = " ";
+		for(int i =0; i<args.length; i++)
+		{
+		sum = sum + Integer.parseInt(args[i]);
+		sum1 = sum1 + args[i];
+		}
+    System.out.println(sum);
+    System.out.println(sum1); 
+}
+}
