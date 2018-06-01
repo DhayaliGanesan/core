341518fcb4917c2fae0eef9eab9a11e050a8d430
DhayaliGanesan <deepadhayali13@gmail.com>
2018-06-01 09:15:48 -0700

    super

---
+++ Car.java
@@ -0,0 +1,18 @@
+package javaproject;
+
+
+public class Car {
+	
+	public Car()
+	{
+		System.out.println("car cons 1");
+	}
+	public Car(int i)
+	{
+		System.out.println("car cons 2");
+	}
+	
+
+}
+
+
