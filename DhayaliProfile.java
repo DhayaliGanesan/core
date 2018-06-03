a68cd3cf9e6b6de5d53758bb5e60385da687b2cb
DhayaliGanesan <deepadhayali13@gmail.com>
2018-06-03 03:51:08 -0700

    profile

---
+++ DhayaliProfile.java
@@ -0,0 +1,21 @@
+package com.personal;
+
+public class DhayaliProfile implements IProfile{
+
+	@Override
+	public void myBasicInfo() {
+		// TODO Auto-generated method stub
+		System.out.println(ProfileConstants.FIRST_NAME+"Dhayali");
+		System.out.println(ProfileConstants.LAST_NAME +"Ganesan");
+		System.out.println(" 21");
+		
+	}
+
+	@Override
+	public void myHobbies() {
+		// TODO Auto-generated method stub
+		System.out.println("My hobby is watching tv");
+	}
+	
+
+}
